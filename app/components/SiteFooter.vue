ae337f09e5503701497a37b39a3483bce7df60f6
FuxuanNet <155711022+FuxuanNet@users.noreply.github.com>
2025-01-13 23:32:42 +0800

    更改页脚链接

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -50,9 +50,9 @@ const { public: configPublic } = useRuntimeConfig();
 const { siteIcp, version } = configPublic;
 
 const linkData = {
-  github: "https://github.com/imsyy/site-status",
-  home: "https://www.imsyy.top",
-  email: "mailto:one@imsyy.top",
+  github: "https://github.com/heuctf",
+  home: "https://talk.orgctf.us.kg/",
+  email: "https://alist.orgctf.us.kg/",
 };
 </script>
 
